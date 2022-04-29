6117cfde03318c910ba86e7d5ecae2ac0674d363
Aloke Tewary <aloketewary@gmail.com>
2022-04-29 11:56:02 +0530

    #4: DateTime clone and convert api added

--- src/lib/datetime.spec.ts
+++ src/lib/datetime.spec.ts
@@ -5,3 +5,14 @@ import { DateTime } from './datetime';
 test('DateTime() is same as js Date()', async (t) => {
   t.is(new DateTime().toDateString(), new Date().toDateString());
 });
+
+test('DateTime() convert from JS Date()', async (t) => {
+  const jsDate = new Date(2022, 0o3, 10);
+  const dateTime = DateTime.convert(jsDate);
+  t.is(dateTime.toDateString(), jsDate.toDateString());
+});
+
+test('DateTime() clone() is same', async (t) => {
+  const dateTime = new DateTime();
+  t.is(dateTime.clone.toDateString(), dateTime.toDateString());
+});
